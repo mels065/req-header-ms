ff91192cdee05518c3e8407154b8bafec095c458
Brandon Mellus <mellusbrandon@gmail.com>
2016-02-23 14:51:49 -0500

    Return IP Address

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function(req, res) {
+    var result = {
+        'IPaddress': req.ip
+    };
+    
+    res.json(result);
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
